a75d1f95d9fb7a1e88a6a8610d794c5e9f0ad7ac
charlesonunze <charlesonunze2@gmail.com>
2020-05-24 23:53:36 +0100

    feat: 🎸 added the commander package for cli things

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import program from 'commander';
 import BackTester from './tester';
+
+program
+  .version('1.0.0')
+  .option('-t, --time [time]', 'How much historical data to use', '1m')
+  .option('-s, --strategy [strategy]', 'Strategy Type', 'simple')
+  .option('-f, --funds [funds]', 'Amount of money to use', parseFloat)
+  .option('-l, --live', 'Run live')
+  .parse(process.argv);
 
 const main = async function () {
   const tester = new BackTester();
