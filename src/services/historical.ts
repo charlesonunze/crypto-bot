458af41fe4a86344223e8445dee27db65d08e7d4
charlesonunze <charlesonunze2@gmail.com>
2020-05-24 23:32:31 +0100

    refactor: 💡 modified historical service and added a new util fn
    Modified the historical service class and added a new util function for
    returning time in milliseconds.
    
    BREAKING CHANGE: 🧨 the getData method is no longer a static method

--- src/services/historical.ts
+++ src/services/historical.ts
@@ -1,23 +1,23 @@
 import API from 'binance';
 import { BINANCE_API_KEY, BINANCE_API_SECRET } from '../config';
 import { CandleStick, CurrencyPair, CandlestickInterval } from '../types';
+import { toMilliSeconds } from '../utils';
 
 const binance = new API.BinanceRest({
   key: BINANCE_API_KEY,
-  secret: BINANCE_API_SECRET,
+  secret: BINANCE_API_SECRET
 });
 
 class HistoricalService {
-  static async getData() {
-    const lastMonth = Date.now() - 2628000000;
-    const now = Date.now();
+  constructor(public startTime: string) {}
 
+  async getData() {
     const data: CandleStick[] = await binance.klines({
       symbol: CurrencyPair.BTC_USD,
       interval: CandlestickInterval.ONE_MINUTE,
-      startTime: lastMonth,
-      endTime: now,
-      limit: 1000,
+      startTime: toMilliSeconds(this.startTime),
+      endTime: Date.now(),
+      limit: 1000
     });
 
     return data;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,23 @@
 export const genRandomString = (length = 6) =>
-	Math.random().toString(20).substr(2, 6);
+  Math.random().toString(20).substr(2, length);
+
+export const toMilliSeconds = (time: string) => {
+  const _time = time.split('');
+  const str = _time.pop();
+  const num = parseInt(_time.join(''));
+
+  if (isNaN(num)) {
+    throw new Error('Enter a valid format');
+  }
+
+  switch (str) {
+    case 'd':
+      return num * 86400000;
+
+    case 'm':
+      return num * 2592000000;
+
+    case 'y':
+      return num * 31536000000;
+  }
+};
